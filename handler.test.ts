664ef197d351e55afbb3ee61be88d3661719e21c
Thomas Gormley <thomas.gormley@firstup.io>
2023-03-04 20:26:41 +0000

    simplify

--- handler.test.ts
+++ handler.test.ts
@@ -1,10 +1,10 @@
 import { expect, it, vi } from "vitest";
 import { handler } from "./handler";
-import * as gitSpy from "./string";
+import * as stringSpy from "./string";
 // import * as outsideSpy from "./get-string";
 
 it("should mock impl", async () => {
-  vi.spyOn(gitSpy, "getString").mockReturnValue("return this");
+  vi.spyOn(stringSpy, "getString").mockReturnValue("return this");
   const consoleSpy = vi.spyOn(console, "log");
   await handler();
 
